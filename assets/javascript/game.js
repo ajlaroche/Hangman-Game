b0c697132bce9a186ef3f0587205952989c83c1a
Antony Laroche <antony@larochefamily.net>
2018-02-10 00:39:53 -0600

    Added reset at end of game and saving guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,18 @@
 var wordLibrary = ["horse", "cowboy", "shotgun", "ranch", "cattle", "longhorn", "aggie", "rodeo"];
 
-var guessesAllowed = 25;
-var guessesRemaining = guessesAllowed;
-
-var chosenWord = wordLibrary[Math.floor(Math.random() * wordLibrary.length)];
-console.log(chosenWord);
-
-//Breakup the chosen word into array of letters for testing user entries
-var chosenWordBreak = [];
-for (var x = 0; x < chosenWord.length; x++) {
-    chosenWordBreak[x] = chosenWord.charAt(x);
-}
-
-
-var hiddenWord = []; //initialize dashes on the screen
-
-for (var i = 0; i < chosenWord.length; i++) {
-    hiddenWord[i] = "_ ";
-}
-
 var initialDashs = "";
+var chosenWord = "";
+var userwordtest = ""; //used to test user word
+var userwordtestresult = "";
+var guessesRemaining = 0;
+var guessesAllowed = 15;
+var hiddenWord = [];
+var chosenWordBreak = [];
+var userLetterGuess;
+var numberOfWins = 0;
+var savedGuesses = "";
+var savedGuessesTest = "";
+var checkGuesses = [];
 
 //Function to print dashes and with correct guesses on screen
 function printGuess() {
@@ -29,14 +22,48 @@ function printGuess() {
     return initialDashs;
 }
 
-printGuess(); //Prints initial dashes on screen
+function resetsheet() {
+
+    guessesRemaining = guessesAllowed;
+    savedGuesses = "";
+
+    chosenWord = wordLibrary[Math.floor(Math.random() * wordLibrary.length)];
+    console.log(chosenWord);
+
+    //Breakup the chosen word into array of letters for testing user entries
+
+    for (var x = 0; x < chosenWord.length; x++) {
+        chosenWordBreak[x] = chosenWord.charAt(x);
+    }
+
+    hiddenWord = []; //used to reset the dashes
+
+    for (var i = 0; i < chosenWord.length; i++) {
+        hiddenWord[i] = "_ ";
+    }
+    initialDashs = "";
+
+    printGuess(); //Prints initial dashes on screen
+
+}
+
+resetsheet();
 
 document.getElementById("currentWord").innerHTML = initialDashs;
+document.getElementById("guessesleft").innerHTML = " " + guessesAllowed;
+document.getElementById("numberOfwins").innerHTML = numberOfWins + " Wins";
 
-var userLetterGuess;
 document.onkeyup = function (event) {
     userLetterGuess = event.key.toLowerCase();
-    guessesRemaining--;
+    savedGuesses = savedGuesses + " " + userLetterGuess.toUpperCase() + ",";
+    savedGuessesTest = savedGuessesTest + userLetterGuess;
+    // for (var z = 0; z < savedGuessesTest.length; z++) {
+    //     checkGuesses[z] = savedGuessesTest.charAt(z);
+    //     if (userLetterGuess !== checkGuesses[z]) {
+    //         guessesRemaining--
+    //     }
+    // }
+    guessesRemaining--
     for (var k = 0; k < hiddenWord.length; k++) {
         if (userLetterGuess === chosenWordBreak[k]) {
             hiddenWord[k] = userLetterGuess;
@@ -44,8 +71,25 @@ document.onkeyup = function (event) {
         initialDashs = ""; //re-initialize dashes to be printed on the screen
         printGuess(); //updates screen with user guess
     }
-
+    for (var y = 0; y < hiddenWord.length; y++) {
+        userwordtest = userwordtest + hiddenWord[y];
+    }
     document.getElementById("currentWord").innerHTML = initialDashs;
     document.getElementById("guessesleft").innerHTML = " " + guessesRemaining;
-}
+    document.getElementById("LetterGuesses").innerHTML = savedGuesses;
+    userwordtestresult = userwordtest;
+    userwordtest = "";
 
+    if (userwordtestresult === chosenWord) {
+        numberOfWins++;
+        resetsheet();
+        document.getElementById("currentWord").innerHTML = initialDashs;
+        document.getElementById("guessesleft").innerHTML = " " + guessesAllowed;
+        document.getElementById("numberOfwins").innerHTML = numberOfWins + " Wins";
+        document.getElementById("Intro").innerHTML = "Press Any Key to Get Started!";
+        document.getElementById("LetterGuesses").innerHTML = savedGuesses;
+
+    } else {
+        document.getElementById("Intro").innerHTML = "Choose Another Letter";
+    }
+}
